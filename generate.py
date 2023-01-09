29fae2ac66a99d269c775297d5e3d8b9cd156632
krishnasorna <krishnasorna2025@u.northwestern.edu>
2023-01-08 20:02:27 -0600

    finished

--- generate.py
+++ generate.py
@@ -24,10 +24,14 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso",
-                       child="Leg", type="revolute", position=[0, 0, 0])
-    pyrosim.Send_Cube(name="Leg", pos=[0, 0, 1.5], size=[1, 1, 1])
+
+    pyrosim.Send_Cube(name="BackLeg", pos=[0, 0.5, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg",
+                       child="Torso", type="revolute", position=[0, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0.5, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso",
+                       child="FrontLeg", type="revolute", position=[0, 1, 0])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, -0.5], size=[1, 1, 1])
     pyrosim.End()
 
 
