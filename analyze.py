23967c736208ccde42ca1a69bfce9d0d1d41f9d7
krishnasorna <krishnasorna2025@u.northwestern.edu>
2023-01-10 14:52:45 -0600

    more commit

--- analyze.py
+++ analyze.py
@@ -1,1 +1,11 @@
 import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+
+matplotlib.pyplot.plot(backLegSensorValues, label = "backLeg")
+matplotlib.pyplot.plot(frontLegSensorValues, label = "frontLeg")
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
+
